c32acec312cb1feb8c9958ef62e74a123b415117
Clifford <bakutarb@gmail.com>
2023-11-05 14:52:23 +0300

    pop-up

--- scripts.js
+++ scripts.js
@@ -14,3 +14,28 @@ document.addEventListener('DOMContentLoaded', function () {
         expanded = !expanded;
     });
 });
+
+const saveButton = document.getElementById('save-button');
+const heartIcon = saveButton.querySelector('i');
+
+saveButton.addEventListener('click', function() {
+    heartIcon.classList.toggle('red-heart');
+});
+
+const purchaseButton = document.querySelector('.purchase-button');
+const popup = document.getElementById('popup');
+const closePopup = document.getElementById('close-popup');
+
+purchaseButton.addEventListener('click', function() {
+    popup.style.display = 'block';
+});
+
+closePopup.addEventListener('click', function() {
+    popup.style.display = 'none';
+});
+
+popup.addEventListener('click', function(event) {
+    if (event.target === popup) {
+        popup.style.display = 'none';
+    }
+});
